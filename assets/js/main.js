77ffcdaa0febd0fb92daac60a78af03e7f4ebcfd
pravinsamr1 <pravinsamr@gmail.com>
2026-07-18 18:06:56 +0530

    feat: update program and ISO pages with specific assets and remove mouse follower styles from CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -692,65 +692,6 @@ CSS TABLE OF CONTENTS
 		});
 		//--- Custom Tilt Js End ---//
 
-		// Mouse Follower
-		// Mouse Follower
-const follower = document.querySelector(
-	".mouse-follower .cursor-outline"
-);
-const dot = document.querySelector(".mouse-follower .cursor-dot");
-
-if (follower && dot) {
-	window.addEventListener("mousemove", (e) => {
-		follower.animate(
-			[
-				{
-					opacity: 1,
-					left: `${e.clientX}px`,
-					top: `${e.clientY}px`,
-					easing: "ease-in-out",
-				},
-			],
-			{
-				duration: 3000,
-				fill: "forwards",
-			}
-		);
-
-		dot.animate(
-			[
-				{
-					opacity: 1,
-					left: `${e.clientX}px`,
-					top: `${e.clientY}px`,
-					easing: "ease-in-out",
-				},
-			],
-			{
-				duration: 1500,
-				fill: "forwards",
-			}
-		);
-	});
-}
-
-		// Mouse Follower Hide Function
-		$("a, button").on("mouseenter mouseleave", function () {
-			$(".mouse-follower").toggleClass("hide-cursor");
-		});
-
-		var terElement = $(
-			"h1, h2, h3, h4, .display-one, .display-two, .display-three, .display-four, .display-five, .display-six"
-		);
-		$(terElement).on("mouseenter mouseleave", function () {
-			$(".mouse-follower").toggleClass("highlight-cursor-head");
-			$(this).toggleClass("highlight-cursor-head");
-		});
-
-		var terElement = $("p");
-		$(terElement).on("mouseenter mouseleave", function () {
-			$(".mouse-follower").toggleClass("highlight-cursor-para");
-			$(this).toggleClass("highlight-cursor-para");
-		});
 
 		//-- Use Gsap Animation --//
 		// Visible From Right Animation
